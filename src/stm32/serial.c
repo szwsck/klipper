ab1326f6b0cd7baf1dde9609228fc4129f270f9f
szwsck <sz.wsck@gmail.com>
2023-07-10 10:36:04 +0200

    modify stats command to check uart

--- src/stm32/serial.c
+++ src/stm32/serial.c
@@ -47,7 +47,13 @@
   #define GPIO_Rx GPIO('D', 9)
   #define GPIO_Tx GPIO('D', 8)
   #define USARTx USART3
-  #define USARTx_IRQn USART3_IRQn
+  #define USARTx_IRQn USART3_IRQ
+#elif CONFIG_STM32_SERIAL_UART4_ALT_PC11_PC10
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PC11,PC10");
+  #define GPIO_Rx GPIO('C', 11)
+  #define GPIO_Tx GPIO('C', 10)
+  #define USARTx UART4
+  #define USARTx_IRQn UART4_IRQn
 #endif
 
 #define CR1_FLAGS (USART_CR1_UE | USART_CR1_RE | USART_CR1_TE   \
